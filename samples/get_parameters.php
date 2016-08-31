3723d42be4316bf3bab20be95972814defda8538
Ahmed Bahet <bahetahmed@gmail.com>
2016-08-31 16:31:49 +0200

    Added saturday delivery, Fix for locale on account creation, Added function for API key generation, Fix for user details + removed parameters in construct + updated doc

--- samples/get_parameters.php
+++ samples/get_parameters.php
@@ -1,28 +1,22 @@
 <?php
-use \Emc\OrderStatus;
+use \Emc\Parameters;
 
-/* Example of use for OrderStatus class
- * Get the status of a passed order
+/* Example of use for Parameters class
+ * Load all available Parameters
  */
+
 require_once('../config/autoload.php');
 require_once(EMC_PARENT_DIR.'layout/header.php');
 
 
-if (EMC_MODE == "prod") {
-    $emcRef = "1605122984DHLEQA2DFR";
-} else {
-    $emcRef = "1606073393UPSE082ZFR";
-}
+/* Prepare and execute the request */
+$lib = new Parameters();
 
-
-// Prepare and execute the request
-$lib = new OrderStatus();
-$lib->getOrderInformations($emcRef);
-
-echo "<h3>API OrderStatus :</h3>";
+$lib->getParameters();
+echo "<h3>API News :</h3>";
 if (!$lib->curl_error && !$lib->resp_error) {
     // If you want to recieve order's status changes or documents, check for "url_push" param in samples/make_order.php
-    echo '<pre>' . print_r($lib->order_info, true) . '</pre>';
+    echo '<pre>' . print_r($lib->parameters, true) . '</pre>';
 } else {
     echo '<div class="alert alert-danger">';
     handle_errors($lib);
@@ -34,7 +28,7 @@ if (!$lib->curl_error && !$lib->resp_error) {
         Toggle Debug
     </button>
     <pre id="debug" style="display: none">
-        <?php print_r(array_merge($lib->getApiParam(), array('API response :' => $lib->order_info))); ?>
+        <?php print_r(array_merge($lib->getApiParam(), array('API response :' => $lib->parameters))); ?>
     </pre>
 </div>
 <?php
