ea62d73bc5d02049b1e1b51815664740e522d5fd
ojomasta <ojomasta@hotmail.com>
2017-11-03 14:50:31 +0100

    Update autoload.php
    autoloader fixe

--- config/autoload.php
+++ config/autoload.php
@@ -1,8 +1,24 @@
 <?php
 use \Emc\Autoloader;
 
-require_once('config.php');
+require_once(__DIR__.'/config.php');
+
+require_once __DIR__.'/../Emc/WebService.php';
+require_once __DIR__.'/../Emc/CarriersList.php';
+require_once __DIR__.'/../Emc/ContentCategory.php';
+require_once __DIR__.'/../Emc/Country.php';
+require_once __DIR__.'/../Emc/ListPoints.php';
+require_once __DIR__.'/../Emc/News.php';
+require_once __DIR__.'/../Emc/OrderStatus.php';
+require_once __DIR__.'/../Emc/Parameters.php';
+require_once __DIR__.'/../Emc/ParcelPoint.php';
+require_once __DIR__.'/../Emc/Quotation.php';
+require_once __DIR__.'/../Emc/QuotationMulti.php';
+require_once __DIR__.'/../Emc/User.php';
+require_once __DIR__.'/../Emc/Carrier.php';
+require_once __DIR__.'/../Emc/Service.php';
 
 
-require(__DIR__.'/../Emc/Autoloader.php');
-Autoloader::register();
+//require(__DIR__.'/../Emc/Autoloader.php');
+
+//Autoloader::register();
